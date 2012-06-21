de2d557fd3910ce6f7527ff8faea9ba5db81351f
Soeren Pottberg <soerenpottberg@web.de>
2012-06-21 09:48:53 +0200

    Benchmark

--- src/main/Benchmark.java
+++ src/main/Benchmark.java
@@ -1,12 +1,61 @@
 package main;
 
+import java.util.Arrays;
+
+import sortingalgorithm.ISortingAlgorithm;
+import sortingalgorithm.InsertionSort;
+import sortingalgorithm.SelectionSort;
+
 public class Benchmark {
 
+	static final int[] sizes = {10};
+	static ISortingAlgorithm[] sortingAlgorithms = new ISortingAlgorithm[2];
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		sortingAlgorithms[0] = new InsertionSort();
+		sortingAlgorithms[1] = new SelectionSort();
+
+		int[] list;
+
+		for (ISortingAlgorithm algorithm : sortingAlgorithms) {
+
+			System.out.println(algorithm.getClass().getSimpleName() + ": ");
+
+			for (int size : sizes) {
+				
+				System.out.println(size);
+
+				// Sortierte Liste
+				list = Universum.createSortedList(size);
+				System.out.println(Arrays.toString(list));
+				algorithm.sort(list);
+				System.out.println(Arrays.toString(list));
+				System.out.println(algorithm.getResultOfTimeMeasurement() + " ");
+
+				// Inverse Liste
+				list = Universum.createInversList(size);
+				System.out.println(Arrays.toString(list));
+				algorithm.sort(list);
+				System.out.println(Arrays.toString(list));
+				System.out.println(algorithm.getResultOfTimeMeasurement()+ " ");
+
+				// Zufällige Liste
+				for (int i = 0; i < 5; i++) {
+					list = Universum.createRandomList(size);
+					System.out.println(Arrays.toString(list));
+					algorithm.sort(list);
+					System.out.println(Arrays.toString(list));
+					System.out.println(algorithm.getResultOfTimeMeasurement() + " ");
+				}
+
+			}			
+		}
+
+		System.out.println("\n");
 
 	}
 
--- src/main/Universum.java
+++ src/main/Universum.java
@@ -1,5 +1,44 @@
 package main;
 
+import java.util.Random;
+
 public class Universum {
+
+	public static int[] createSortedList(int size) {
+		
+		int[] list = new int[size];
+		
+		for (int i = 0; i < list.length; i++) {
+			list[i] = i;
+		}
+		
+		return list;
+		
+	}
+
+	public static int[] createInversList(int size) {
+		
+		int[] list = new int[size];
+		
+		for (int i = 0; i < list.length; i++) {
+			list[i] = list.length - i;
+		}
+		
+		return list;
+		
+	}
+
+	public static int[] createRandomList(int size) {
+		
+		int[] list = new int[size];
+		Random random = new Random();
+		
+		for (int i = 0; i < list.length; i++) {
+			list[i] = random.nextInt();
+		}
+		
+		return list;
+		
+	}
 
 }
